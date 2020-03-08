deea8af6708968d9b8eb677ec0579dbd893c124b
robert <rob@hawk.com>
2020-03-07 22:04:20 -0700

    Primera subida de archivos

---
+++ KarelOs/MantenimientoCliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KarelOs
+{
+    public partial class MantenimientoCliente : Mantenimiento
+    {
+        public MantenimientoCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void MantenimientoCliente_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ KarelOs/MantenimientoProducto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MiLibreria;
+namespace KarelOs
+{
+    public partial class MantenimientoProducto : Mantenimiento
+    {
+        public MantenimientoProducto()
+        {
+            InitializeComponent();
+        }
+
+        private void MantenimientoProducto_Load(object sender, EventArgs e)
+        {
+            string cmd = "SELECT * FROM articulos";
+            DataTable ds = Utilerias.LlenarGrid(cmd);
+
+            dataGridView1.DataSource = ds;
+            
+        }
+
+        public override Boolean Guardar()
+        {
+            try
+            {
+                string cmd = string.Format("CALL ActualizarArticulos('{0}','{1}')", txtNombreProducto.Text.Trim(), txtPrecioProducto.Text.Trim());
+                Utilerias.Ejecutar(cmd);
+                MessageBox.Show("Registro Exitoso");
+                return true;
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public override void Eliminar()
+        {
+            try
+            {
+                string cmd = string.Format("CALL EliminarArticulo('{0}')", txtNombreProducto.Text.Trim());
+                Utilerias.Ejecutar(cmd);
+                MessageBox.Show("Registro Eliminado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
