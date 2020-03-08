435c7bfc34dca2bb649524b80ae474496686d4fa
robert <rob@hawk.com>
2020-03-07 22:09:10 -0700

    Etapata 1

---
+++ KarelOs/MantenimientoCliente.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace KarelOs
+{
+    partial class MantenimientoCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtApellidoPCliente = new System.Windows.Forms.TextBox();
+            this.txtNombreCliente = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtApellidoMCliente = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(448, 36);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(448, 72);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(448, 108);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(448, 144);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(448, 216);
+            // 
+            // txtApellidoPCliente
+            // 
+            this.txtApellidoPCliente.Location = new System.Drawing.Point(203, 76);
+            this.txtApellidoPCliente.Name = "txtApellidoPCliente";
+            this.txtApellidoPCliente.Size = new System.Drawing.Size(174, 22);
+            this.txtApellidoPCliente.TabIndex = 12;
+            // 
+            // txtNombreCliente
+            // 
+            this.txtNombreCliente.Location = new System.Drawing.Point(203, 43);
+            this.txtNombreCliente.Name = "txtNombreCliente";
+            this.txtNombreCliente.Size = new System.Drawing.Size(174, 22);
+            this.txtNombreCliente.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Apellido Paterno";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nombre";
+            // 
+            // txtApellidoMCliente
+            // 
+            this.txtApellidoMCliente.Location = new System.Drawing.Point(203, 112);
+            this.txtApellidoMCliente.Name = "txtApellidoMCliente";
+            this.txtApellidoMCliente.Size = new System.Drawing.Size(174, 22);
+            this.txtApellidoMCliente.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(42, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 25);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Apellido Materno";
+            // 
+            // MantenimientoCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 349);
+            this.Controls.Add(this.txtApellidoMCliente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtApellidoPCliente);
+            this.Controls.Add(this.txtNombreCliente);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MantenimientoCliente";
+            this.Text = "MantenimientoCliente";
+            this.Load += new System.EventHandler(this.MantenimientoCliente_Load);
+            this.Controls.SetChildIndex(this.btnSalir, 0);
+            this.Controls.SetChildIndex(this.btnConsultar, 0);
+            this.Controls.SetChildIndex(this.btnGuardar, 0);
+            this.Controls.SetChildIndex(this.btnEliminar, 0);
+            this.Controls.SetChildIndex(this.btnNuevo, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.txtNombreCliente, 0);
+            this.Controls.SetChildIndex(this.txtApellidoPCliente, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.txtApellidoMCliente, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtApellidoPCliente;
+        private System.Windows.Forms.TextBox txtNombreCliente;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtApellidoMCliente;
+        private System.Windows.Forms.Label label3;
+    }
+}
